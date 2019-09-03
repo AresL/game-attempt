5c3a66c10f78425315dd149a9f31a5f7220b6310
Ares <ares.li@outlook.com>
2019-09-03 10:36:30 +0200

    Initial script

--- game attempt 1/Assets/Scripts/addForce.cs
+++ game attempt 1/Assets/Scripts/addForce.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class addForce : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float thrust = 1.5F;
+    public Rigidbody rb;
+    public bool isTapable = true;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(rb);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collision collision)
     {
-        
+        Debug.Log("OnCollisionEnter");
+        isTapable = true;
+    }
+
+    private void OnTriggerExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit");
+        isTapable = false;
+    }
+    private void OnMouseDown()
+    {
+        if (isTapable)
+        {
+            Debug.Log("Tap!");
+
+            rb.AddForce(transform.up * thrust);
+
+            isTapable = false;
+        }
+        else
+        {
+            Debug.Log("Not tapable");
+        }
     }
 }
